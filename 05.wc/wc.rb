2427660043c895f20c5c92c1b8fae2df8bf94bbf
tyrrell-IH <gotothertm@gmail.com>
2023-09-04 00:19:02 +0900

    rubocop指摘事項の修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/env ruby
 # frozen_string_literal: true
 
-#列の幅が8の倍数になる
+# 列の幅が8の倍数になる
 MULTIPLE_OF_COLUMN_WIDTH = 8
 
 def main
@@ -21,7 +21,7 @@ end
 
 def count_words(file)
   words = File.read(file).split(/\s/)
-  words.reject{|word| word.length.zero?}.size
+  words.count { |word| !word.empty? }
 end
 
 def count_bytes(file)
@@ -34,7 +34,7 @@ end
 
 def count_stdin_words(text)
   words = text.split(/\s/)
-  words.reject{|word| word.length.zero?}.size
+  words.count { |word| !word.empty? }
 end
 
 def count_stdin_bytes(text)
@@ -42,19 +42,19 @@ def count_stdin_bytes(text)
 end
 
 def get_lines_width(files)
-  max_file = files.max_by { |file| count_lines(file).to_s.length}
+  max_file = files.max_by { |file| count_lines(file).to_s.length }
   max_length = count_lines(max_file).to_s.length
   (max_length.next..).find { |n| (n % MULTIPLE_OF_COLUMN_WIDTH).zero? }
 end
 
 def get_words_width(files)
-  max_file = files.max_by { |file| count_words(file).to_s.length}
+  max_file = files.max_by { |file| count_words(file).to_s.length }
   max_length = count_words(max_file).to_s.length
   (max_length.next..).find { |n| (n % MULTIPLE_OF_COLUMN_WIDTH).zero? }
 end
 
 def get_bytes_width(files)
-  max_file = files.max_by { |file| count_bytes(file).to_s}
+  max_file = files.max_by { |file| count_bytes(file).to_s }
   max_length = count_bytes(max_file).to_s.length
   (max_length.next..).find { |n| (n % MULTIPLE_OF_COLUMN_WIDTH).zero? }
 end
@@ -78,13 +78,13 @@ def generate_contents(files)
   lines_width = get_lines_width(files)
   words_width = get_words_width(files)
   bytes_width = get_bytes_width(files)
-  contents = files.map do |file|
+  files.map do |file|
     [
-    count_lines(file).to_s.rjust(lines_width),
-    count_words(file).to_s.rjust(words_width),
-    count_bytes(file).to_s.rjust(bytes_width),
-    " #{file}"
-  ].join('')
+      count_lines(file).to_s.rjust(lines_width),
+      count_words(file).to_s.rjust(words_width),
+      count_bytes(file).to_s.rjust(bytes_width),
+      " #{file}"
+    ].join('')
   end
 end
 
@@ -92,15 +92,15 @@ def generate_total_contents(files)
   lines_width = get_lines_width(files)
   words_width = get_words_width(files)
   bytes_width = get_bytes_width(files)
-  lines_total = files.inject(0) { |result, file| result + count_lines(file)}
-  words_total = files.inject(0) { |result, file| result + count_words(file)}
-  bytes_total = files.inject(0) { |result, file| result + count_bytes(file)}
+  lines_total = files.inject(0) { |result, file| result + count_lines(file) }
+  words_total = files.inject(0) { |result, file| result + count_words(file) }
+  bytes_total = files.inject(0) { |result, file| result + count_bytes(file) }
   [
-  lines_total.to_s.rjust(lines_width),
-  words_total.to_s.rjust(words_width),
-  bytes_total.to_s.rjust(bytes_width),
-  " total"
-].join('')
+    lines_total.to_s.rjust(lines_width),
+    words_total.to_s.rjust(words_width),
+    bytes_total.to_s.rjust(bytes_width),
+    ' total'
+  ].join('')
 end
 
 def generate_stdin_contents(text)
@@ -111,7 +111,7 @@ def generate_stdin_contents(text)
     count_stdin_lines(text).to_s.rjust(lines_width),
     count_stdin_words(text).to_s.rjust(words_width),
     count_stdin_bytes(text).to_s.rjust(bytes_width)
-].join('')
+  ].join('')
 end
 
 main
