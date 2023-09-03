f2c8677e09a73b89180f65adfbdecf316f74280a
tyrrell-IH <gotothertm@gmail.com>
2023-09-03 19:58:08 +0900

    ファイルが指定された際の表示するcontents（total以外)を作成

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,28 +1,50 @@
 #! /usr/bin/env ruby
 # frozen_string_literal: true
 
-def make_text(file)
-  File.read(file)
+#列の幅が8の倍数になる
+MULTIPLE_OF_COLUMN_WIDTH = 8
+
+def count_lines(file)
+  File.read(file).lines.size.to_s
 end
 
-def count_lines(text)
-  text.lines.size
+def count_words(file)
+  words = File.read(file).split(/\s/)
+  words.reject{|word| word.length.zero?}.size.to_s
 end
 
-def count_words(text)
-  text.split(/\s/).reject{|word| word.length.zero?}.size
+def count_bytes(file)
+  File.read(file).bytesize.to_s
 end
 
-def count_bytes(text)
-  text.bytesize
+def get_lines_width(files)
+  max_file = files.max_by { |file| count_lines(file)}
+  max_length = count_lines(max_file).length
+  (max_length.next..).find { |n| (n % MULTIPLE_OF_COLUMN_WIDTH).zero? }
 end
 
-if ARGV.empty?
-  text = $stdin.read
-  puts " #{count_lines(text)} #{count_words(text)} #{count_bytes(text)}"
-else
-  ARGV.each do |file|
-    text = make_text(file)
-    puts " #{count_lines(text)} #{count_words(text)} #{count_bytes(text)} #{file}"
+def get_words_width(files)
+  max_file = files.max_by { |file| count_words(file)}
+  max_length = count_words(max_file).length
+  (max_length.next..).find { |n| (n % MULTIPLE_OF_COLUMN_WIDTH).zero? }
+end
+
+def get_bytes_width(files)
+  max_file = files.max_by { |file| count_bytes(file)}
+  max_length = count_bytes(max_file).length
+  (max_length.next..).find { |n| (n % MULTIPLE_OF_COLUMN_WIDTH).zero? }
+end
+
+def generate_contents(files)
+  lines_width = get_lines_width(files)
+  words_width = get_words_width(files)
+  bytes_width = get_bytes_width(files)
+  contents = files.map do |file|
+    [
+      count_lines(file).rjust(lines_width),
+      count_words(file).rjust(words_width),
+      count_bytes(file).rjust(bytes_width),
+      " #{file}"
+  ]
   end
 end
