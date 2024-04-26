e80da90ee119a26e89d66e8e89bb11e789dfd846
tyrrell-IH <gotothertm@gmail.com>
2024-04-26 13:29:11 +0900

    ボウリングプログラムの下地作成。与えられたスコアを合計するのみの仕様

---
+++ 06.bowling_object/game.rb
@@ -0,0 +1,22 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative 'shot'
+
+class Game
+  attr_reader :total_score
+  def initialize(scores)
+    @scores = scores
+    @total_score = 0
+  end
+  def add_scores_to_total
+    @scores.each do |score|
+      @total_score += Shot.new(score).score
+    end
+  end
+end
+
+scores = ARGV[0].split(',')
+game = Game.new(scores)
+game.add_scores_to_total
+puts game.total_score
