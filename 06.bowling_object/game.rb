7ea71c2924303fc73431181bc458331ed2a6e9b8
tyrrell-IH <gotothertm@gmail.com>
2024-04-28 07:45:22 +0900

    game.rb内のトータルスコアの算出をFrameクラスを利用して算出するよう修正

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -1,23 +1,14 @@
 # frozen_string_literal: true
 
-require_relative 'shot'
+require_relative 'frame'
 
 class Game
   attr_reader :total_score
 
   def initialize(scores)
-    @scores = scores
     @total_score = 0
-  end
-
-  def add_scores_to_total
-    @scores.each do |score|
-      @total_score += Shot.new(score).score
+    scores.each_slice(2).with_index(1) do |frame_score, n|
+      @total_score += Frame.new(first_shot: Shot.new(frame_score[0]), second_shot: Shot.new(frame_score[1]), frame_number: n).score
     end
   end
 end
-
-scores = ARGV[0].split(',')
-game = Game.new(scores)
-game.add_scores_to_total
-puts game.total_score
