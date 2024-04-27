373c73e16774a1362c67f77ec363ff72efb1a479
tyrrell-IH <gotothertm@gmail.com>
2024-04-27 16:59:24 +0900

    shot.rbのrubocop指摘事項修正

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -1,9 +1,9 @@
-#! /usr/bin/env ruby
 # frozen_string_literal: true
 
 class Shot
   attr_reader :score
+
   def initialize(score)
-    @score = (score == 'X') ? 10 : score.to_i
+    @score = score == 'X' ? 10 : score.to_i
   end
 end
