a477d2449de4775649e84e6440e1f1802d1c05d2
tyrrell-IH <gotothertm@gmail.com>
2024-05-01 12:49:35 +0900

    Frameクラスにscoreメソッド追加、及びFrameインスタンス作成時にShotインスタンスを引数として受け取るよう修正

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
-require_relative 'shot'
-
 class Frame
   attr_reader :first_shot, :second_shot, :third_shot, :frame_number
-  attr_accessor :next_frame
 
   def initialize(frame_number:, first_shot:, second_shot: nil, third_shot: nil)
     @frame_number = frame_number
-    @first_shot = Shot.new(first_shot)
-    @second_shot = Shot.new(second_shot) if second_shot
-    @third_shot = Shot.new(third_shot) if third_shot
+    @first_shot = first_shot
+    @second_shot = second_shot if second_shot
+    @third_shot = third_shot if third_shot
   end
 
   def strike?
@@ -22,7 +19,27 @@ class Frame
   end
 
   def score
-    frame_score = @first_shot.score + @second_shot.score
-    @third_shot ? (frame_score + @third_shot.score) : frame_score
+    normal_score + bonus_score
+  end
+
+  private
+
+  def normal_score
+    score = @first_shot.score
+    score += @second_shot.score if @second_shot
+    score += @third_shot.score if @third_shot
+    score
+  end
+
+  def bonus_score
+    return 0 if @frame_number == 10
+
+    if strike?
+      @first_shot.next_shot.score + @first_shot.next_shot.next_shot.score
+    elsif spare?
+      @second_shot.next_shot.score
+    else
+      0
+    end
   end
 end
