cc7fa05b5ab530f9915cea8fae9b8953b1581f1f
tyrrell-IH <gotothertm@gmail.com>
2024-05-01 14:45:32 +0900

    Gameクラスの内容を修正

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -1,25 +1,13 @@
 # frozen_string_literal: true
 
-require_relative 'frame'
-require_relative 'score_arrangement'
+require_relative 'frames_factory'
 
 class Game
-  include ScoreArrangement
-
-  attr_reader :total_score
-
   def initialize(scores)
-    @frames = []
-    scores_each_frames = arrange_scores(scores)
-    scores_each_frames.map.with_index(1) do |frame_score, n|
-      @frames << case frame_score.size
-                 when 1
-                   Frame.new(frame_number: n, first_shot: frame_score[0])
-                 when 2
-                   Frame.new(frame_number: n, first_shot: frame_score[0], second_shot: frame_score[1])
-                 when 3
-                   Frame.new(frame_number: n, first_shot: frame_score[0], second_shot: frame_score[1], third_shot: frame_score[2])
-                 end
-    end
+    @frames = FramesFactory.new(scores).manufacture
+  end
+
+  def total_score
+    @frames.inject(0) { |result, frame| result + frame.score }
   end
 end
