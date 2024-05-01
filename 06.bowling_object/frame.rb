a2dfc04b283ff03f8e1d95bd22ff318a93ed8b06
tyrrell-IH <gotothertm@gmail.com>
2024-05-02 00:54:09 +0900

    Frameクラスに読み書きインスタンス変数next_frame再実装、その他大幅な修正

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -1,45 +1,33 @@
 # frozen_string_literal: true
 
+require_relative 'shot'
 class Frame
   attr_reader :first_shot, :second_shot, :third_shot, :frame_number
+  attr_accessor :next_frame
 
-  def initialize(frame_number:, first_shot:, second_shot: nil, third_shot: nil)
+  def initialize(frame_number:, first_record:, second_record: nil, third_record: nil)
     @frame_number = frame_number
-    @first_shot = first_shot
-    @second_shot = second_shot if second_shot
-    @third_shot = third_shot if third_shot
+    @first_shot = Shot.new(first_record)
+    @second_shot = Shot.new(second_record) if second_record
+    @third_shot = Shot.new(third_record) if third_record
   end
 
   def strike?
+    return nil if @frame_number == 10
+
     @first_shot.score == 10
   end
 
   def spare?
+    return nil if @frame_number == 10
+
     (@first_shot.score != 10) && ((@first_shot.score + @second_shot.score) == 10)
   end
 
   def score
-    normal_score + bonus_score
-  end
-
-  private
-
-  def normal_score
     score = @first_shot.score
     score += @second_shot.score if @second_shot
     score += @third_shot.score if @third_shot
     score
-  end
-
-  def bonus_score
-    return 0 if @frame_number == 10
-
-    if strike?
-      @first_shot.next_score + @first_shot.next_next_score
-    elsif spare?
-      @second_shot.next_score
-    else
-      0
-    end
   end
 end
