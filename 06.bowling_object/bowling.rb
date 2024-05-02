07c799c368d6819781f6c0249a07fec18905a387
tyrrell-IH <gotothertm@gmail.com>
2024-05-03 00:36:24 +0900

    最終的な実行ファイルであるbowling.rbを作成

---
+++ 06.bowling_object/bowling.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative 'game'
+
+def exec
+  records = ARGV[0].split(',')
+  game = Game.new(records)
+  game.generate_frames
+  puts game.total_score
+end
+
+exec
