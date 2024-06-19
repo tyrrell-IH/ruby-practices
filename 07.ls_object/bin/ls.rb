9a3528fd69f5bf651b2b2ae3aec1d33a51b9d61f
tyrrell-IH <gotothertm@gmail.com>
2024-06-19 10:14:58 +0900

    Displayクラスがあまり責務をおっていないのでls.rbと統合

--- 07.ls_object/bin/ls.rb
+++ 07.ls_object/bin/ls.rb
@@ -1,9 +1,14 @@
 #! /usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative '../lib/display'
+require_relative '../lib/multi_column_format'
+require_relative '../lib/long_format'
 require_relative '../lib/option'
 
 file_names = Option.a? ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
 file_names.reverse! if Option.r?
-Display.new(file_names).exec
+if Option.l?
+  puts LongFormat.new(file_names).fit_in
+else
+  puts MultiColumnFormat.new(file_names).fit_in
+end
