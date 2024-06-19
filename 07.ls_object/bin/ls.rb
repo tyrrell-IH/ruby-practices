a8bfbe8d5ea049ff398f2645c29eac0a7ae12e69
tyrrell-IH <gotothertm@gmail.com>
2024-06-19 11:11:36 +0900

    OptionクラスがOptionParserのラッパークラスとなっており、他の明確な責務がないので削除、ls.rbと統合

--- 07.ls_object/bin/ls.rb
+++ 07.ls_object/bin/ls.rb
@@ -3,11 +3,18 @@
 
 require_relative '../lib/multi_column_format'
 require_relative '../lib/long_format'
-require_relative '../lib/option'
+require 'optparse'
 
-file_names = Option.a? ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
-file_names.reverse! if Option.r?
-if Option.l?
+params = {}
+opt = OptionParser.new
+opt.on('-a') { |v| params[:a] = v }
+opt.on('-r') { |v| params[:r] = v }
+opt.on('-l') { |v| params[:l] = v }
+opt.parse!
+
+file_names = params[:a] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+file_names.reverse! if params[:r]
+if params[:l]
   puts LongFormat.new(file_names).fit_in
 else
   puts MultiColumnFormat.new(file_names).fit_in
