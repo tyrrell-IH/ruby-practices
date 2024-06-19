a1e5e17d6916f4b5126b7213db422cd93ca00035
tyrrell-IH <gotothertm@gmail.com>
2024-06-19 12:50:14 +0900

    ls.rb内でベタ打ちのコードが多くなり読みづらくなったので、メソッドにまとめました

--- 07.ls_object/bin/ls.rb
+++ 07.ls_object/bin/ls.rb
@@ -5,17 +5,29 @@ require_relative '../lib/multi_column_format'
 require_relative '../lib/long_format'
 require 'optparse'
 
-params = {}
-opt = OptionParser.new
-opt.on('-a') { |v| params[:a] = v }
-opt.on('-r') { |v| params[:r] = v }
-opt.on('-l') { |v| params[:l] = v }
-opt.parse!
-
-file_names = params[:a] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
-file_names.reverse! if params[:r]
-if params[:l]
-  puts LongFormat.new(file_names).fit_in
-else
-  puts MultiColumnFormat.new(file_names).fit_in
+def main
+  options = parse_options
+  file_names = options[:a] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+  file_names.reverse! if options[:r]
+  choose_format(file_names, l_option: options[:l])
 end
+
+def parse_options
+  params = {}
+  opt = OptionParser.new
+  opt.on('-a') { |v| params[:a] = v }
+  opt.on('-r') { |v| params[:r] = v }
+  opt.on('-l') { |v| params[:l] = v }
+  opt.parse!
+  params
+end
+
+def choose_format(file_names, l_option:)
+  if l_option
+    puts LongFormat.new(file_names).fit_in
+  else
+    puts MultiColumnFormat.new(file_names).fit_in
+  end
+end
+
+main
